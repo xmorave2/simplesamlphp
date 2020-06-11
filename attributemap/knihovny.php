e08f433afe1a168dbde61bf0ba48e1e71e8af07d
Vaclav Rosecky <xrosecky@gmail.com>
2020-06-11 09:16:51 +0200

    attributemap for IdPs hosted by knihovny.cz

---
+++ attributemap/knihovny.php
@@ -0,0 +1,8 @@
+<?php
+$attributemap = array(
+        'commonNameASCII' => 'http://eduid.cz/attributes/commonName#ASCII',
+        'unstructuredName' => 'urn:oid:1.2.840.113549.1.9.2',
+        'userLibraryId' => 'https://shib.knihovny.cz/attribute-def/userLibraryId',
+        'userHomeLibrary' => 'https://shib.knihovny.cz/attribute-def/userHomeLibrary',
+        'authMail' => 'urn:oid:1.2.840.113549.1.9.1',
+);
